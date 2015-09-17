3b7bac0200a1f950fd05185d398b3435940daf3a
Adrien Leloup <nittiyh@outlook.com>
2015-09-17 23:51:34 +0200

    Add google fonts to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,9 @@
 	<title>
 		@yield( "page-title" )
 	</title>
+	<link rel="stylesheet" href="{{ asset( '/css/normalize.css' ) }}" media="screen" title="no title" charset="utf-8">
+	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
 	@yield( "css" )
 </head>
 <body>
