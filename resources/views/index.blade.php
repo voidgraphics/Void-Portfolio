4192f74b8b286e8f58c9b767f4e3fea15c5488a9
Adrien Leloup <nittiyh@outlook.com>
2015-09-17 23:51:45 +0200

    Add index header markup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,5 +9,18 @@ Home - Void Graphics
 @stop
 
 @section( "content" )
-<h1>Yo</h1>
+<header class="header">
+	<div class="header__content">
+		<img src="{{ asset( '/img/logo.svg' ) }}" alt="Void Graphics Logo" class="header__logo--image">
+		<h1 class="header__logo--text">Void graphics</h1>
+		<p class="welcome">
+			<span class="welcome__greeting">Hello!</span>
+			<p class="welcome__about">
+				I'm a Belgian graphics design student specialised in creating beautiful, efficient and fun applications for smartphones, desktop and the web.
+			</p>
+		</p>
+		<a href="" class="contact-btn"><span class="contact-btn__text">Contact me</span><span class="contact-btn__bg"></span></a>
+		<button class="scrolldown-btn"></button>
+	</div>
+</header>
 @stop
