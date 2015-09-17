19c27e22776194dbf27496e57520da73656166f5
Adrien Leloup <nittiyh@outlook.com>
2015-09-17 22:23:46 +0200

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends( "layout" )
+
+@section( "page-title" )
+Home - Void Graphics
+@stop
+
+@section( "css" )
+<link rel="stylesheet" href="{{ asset( '/css/index.css' ) }}">
+@stop
+
+@section( "content" )
+<h1>Yo</h1>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>
+		@yield( "page-title" )
+	</title>
+	@yield( "css" )
+</head>
+<body>
+	@yield( "content" )
+	@yield( "scripts" )
+</body>
+</html>
