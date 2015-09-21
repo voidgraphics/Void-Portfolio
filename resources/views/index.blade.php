01132eb81f4fb6a23985f70817788dc2526cd3e3
Adrien Leloup <nittiyh@outlook.com>
2015-09-21 13:50:19 +0200

    Add about section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,4 +23,28 @@ Home - Void Graphics
 		<button class="scrolldown-btn"></button>
 	</div>
 </header>
+<div class="about">
+	<div class="wrap">
+		<h2 class="about__heading">A few words about me</h2>
+		<div class="row-3 about__info">
+			<h3 class="about__info__heading">Adrien Leloup, 21, Belgian, student.</h3>
+			<p class="about__info__text">
+				I am an ambitious 21 year-old from Belgium.
+	I have a deep passion for crafting experiences for people through technology - be it websites or mobile &amp; desktop apps. My journey started by creating custom themes for Windows: modifying resource images in system files to give the user’s computer a brand new look &amp; feel. I quickly fell in love with this concept, which led me to start studies in this field.
+			</p>
+		</div>
+		<div class="row-3 about__picture">
+			<img src="{{ asset( '/img/face.png' ) }}" alt="" />
+		</div>
+		<div class="row-3 about__info">
+			<h3 class="about__info__heading">My skills</h3>
+			<p class="about__info__text">
+				I am skilled in both front &amp; back end development for the web.
+	My front end skills include HTML, CSS, JavaScript, and the use of front-oriented frameworks such as Jade, SASS &amp; Compass, and CoffeeScript.
+	My back end skills include PHP, node.js and Ruby, including a few frameworks such as Laravel and Rails.
+	I also do iOS and OS X development using Swift.
+			</p>
+		</div>
+	</div>
+</div>
 @stop
