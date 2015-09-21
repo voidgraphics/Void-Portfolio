bc3bcc36f1f3158ae5229475feadbf288bafa7d5
Adrien Leloup <nittiyh@outlook.com>
2015-09-21 13:50:39 +0200

    Add favicon & meta viewport for mobile

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,9 @@
 	<title>
 		@yield( "page-title" )
 	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1"><meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="{{ asset( '/css/normalize.css' ) }}" media="screen" title="no title" charset="utf-8">
+	<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
 	@yield( "css" )
