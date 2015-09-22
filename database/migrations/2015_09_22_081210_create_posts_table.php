f5196a79e7748ef85c438e4357e820d1e2cfb5fb
Adrien Leloup <nittiyh@outlook.com>
2015-09-22 16:33:53 +0200

    Add slug to post migration

--- database/migrations/2015_09_22_081210_create_posts_table.php
+++ database/migrations/2015_09_22_081210_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->string('slug');
             $table->longText('body');
             $table->timestamps();
         });
