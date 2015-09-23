f5c4bf2b3a0f076e8caf5c8282ab05154bf4e11b
Adrien Leloup <nittiyh@outlook.com>
2015-09-23 15:25:11 +0200

    Add sections

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,6 +9,7 @@ Blog: {{ $post->title }} - Void Graphics
 @stop
 
 @section('content')
+<h1 class="title--hidden">Blog: {{ $post->title }}</h1>
 <article>
 	<header class="hero">
 		<div class="hero__content">
@@ -25,39 +26,43 @@ Blog: {{ $post->title }} - Void Graphics
 					{{ $post->body }}
 				</p>
 			</div><div class="viewmore">
-				<h3 class="viewmore__heading">More blog posts</h3>
-				<ul class="viewmore__posts">
-					@foreach($morePosts as $morePost)
-						<li>
-							<article class="blog__post-preview">
-								<header>
-									<h3 class="post__heading">
-										<a href="/posts/{{ $morePost->slug }}" class="post__heading__link">{{ $morePost->title }}</a>
-									</h3>
-									<hr>
-									<p class="post__date">
-										{{ \Carbon\Carbon::instance($morePost->created_at)->toFormattedDateString() }}
-									</p>
-								</header>
-								<main class="post__body">
-									{{ str_limit($morePost->body, 100, '...') }}
-								</main>
-								<footer class="post__footer">
-									<a href="/posts/{{ $morePost->slug }}" class="post__footer__link">Read this blog entry</a>
-								</footer>
-							</article>
-						</li>
-					@endforeach
-				</ul>
-				<h3 class="viewmore__heading">More works</h3>
-				<ul class="viewmore__works">
-					@foreach($moreWorks as $moreWork)
-						<li>
-							<img src="{{ asset($moreWork->thumb_path) }}" alt="" />
-							{{ $moreWork->title }}
-						</li>
-					@endforeach
-				</ul>
+				<section>
+					<h3 class="viewmore__heading">More blog posts</h3>
+					<ul class="viewmore__posts sidebar">
+						@foreach($morePosts as $morePost)
+							<li class="sidebar__item">
+								<article class="blog__post-preview">
+									<header>
+										<h3 class="post__heading">
+											<a href="/posts/{{ $morePost->slug }}" class="post__heading__link">{{ $morePost->title }}</a>
+										</h3>
+										<hr>
+										<p class="post__date">
+											{{ \Carbon\Carbon::instance($morePost->created_at)->toFormattedDateString() }}
+										</p>
+									</header>
+									<main class="post__body">
+										{{ str_limit($morePost->body, 100, '...') }}
+									</main>
+									<footer class="post__footer">
+										<a href="/posts/{{ $morePost->slug }}" class="post__footer__link">Read this blog entry</a>
+									</footer>
+								</article>
+							</li>
+						@endforeach
+					</ul>
+				</section>
+				<section>
+					<h3 class="viewmore__heading">More works</h3>
+					<ul class="viewmore__works">
+						@foreach($moreWorks as $moreWork)
+							<li>
+								<img src="{{ asset($moreWork->thumb_path) }}" alt="" />
+								{{ $moreWork->title }}
+							</li>
+						@endforeach
+					</ul>
+				</section>
 			</div>
 		</div>
 	</main>
