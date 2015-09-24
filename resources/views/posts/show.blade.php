2f95f9a19e2b1a263c3909cc16b68cee9a9429e6
Adrien Leloup <nittiyh@outlook.com>
2015-09-24 10:19:33 +0200

    Finish blog post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,65 +6,92 @@ Blog: {{ $post->title }} - Void Graphics
 
 @section( "css" )
 <link rel="stylesheet" href="{{ asset( '/css/blog-post.css' ) }}">
+<link href="{{ asset( '/js/prettify/src/prettify.css' ) }}" type="text/css" rel="stylesheet" />
 @stop
 
 @section('content')
+<?php use \Michelf\Markdown; ?>
 <h1 class="title--hidden">Blog: {{ $post->title }}</h1>
-<article>
-	<header class="hero">
-		<div class="hero__content">
-			<h2 class="hero__title">{{ $post->title }}</h2>
-			<hr>
-			<p class="hero__date">{{ \Carbon\Carbon::instance($post->created_at)->toFormattedDateString() }}</p>
-		</div>
-		<img src="{{ asset( $post->header_path ) }}" class="hero__bg" alt="" />
-	</header>
-	<main class="post">
-		<div class="wrap">
-			<div class="post__content">
-				<p class="post__paragraph">
-					{{ $post->body }}
+<div class="wrap">
+	<article class="post">
+		<header class="post__header">
+			<h2 class="post__title">{{ $post->title }}</h2>
+		</header>
+		<main class="post__content">
+			<time class="post__date">{{ \Carbon\Carbon::instance($post->created_at)->toFormattedDateString() }}</time>
+			<p class="post__paragraph">
+				{!! Markdown::defaultTransform($post->body) !!}
+			</p>
+		</main>
+	</article>
+</div>
+<section class="recent-works section">
+	<h2 class="section__heading">Have a look...</h2>
+	<div class="wrap">
+		@foreach($moreWorks as $moreWork)
+			<article class="row-3 work-preview">
+				<div class="work-preview__thumb-container">
+					<a href="/works/{{ $moreWork->slug }}" class="work-preview__link">
+						<img src="{{ asset( $moreWork->thumbnail_path ) }}" class="work-preview__thumbnail" alt="" width="100%"/>
+					</a>
+				</div>
+				<h3 class="work-preview__heading">
+					<a href="/works/{{ $moreWork->slug }}" class="work-preview__link">{{ $moreWork->title }}</a>
+				</h3>
+				<hr>
+				<time class="work-preview__date">
+					{{ \Carbon\Carbon::instance($moreWork->created_at)->toFormattedDateString() }}
+				</time>
+			</article>
+		@endforeach
+		<a href="#" class="btn">
+			<span class="btn__text">View more design</span>
+			<span class="btn__bg"></span>
+		</a>
+	</div>
+</section>
+<section class="blog section">
+	<h2 class="section__heading">Here's some more blog posts</h2>
+	<div class="wrap">
+		@foreach($morePosts as $morePost)
+		<article class="row-3 blog__post-preview">
+			<header>
+				<h3 class="post__heading">
+					<a href="/posts/{{ $morePost->slug }}" class="post__heading__link">{{ $morePost->title }}</a>
+				</h3>
+				<hr>
+				<time class="post__date">
+					{{ \Carbon\Carbon::instance($morePost->created_at)->toFormattedDateString() }}
+				</time>
+			</header>
+			<div class="post__body">
+				<p>
+					{{ str_limit($morePost->body, 300, '...') }}
 				</p>
-			</div><div class="viewmore">
-				<section>
-					<h3 class="viewmore__heading">More blog posts</h3>
-					<ul class="viewmore__posts sidebar">
-						@foreach($morePosts as $morePost)
-							<li class="sidebar__item">
-								<article class="blog__post-preview">
-									<header>
-										<h3 class="post__heading">
-											<a href="/posts/{{ $morePost->slug }}" class="post__heading__link">{{ $morePost->title }}</a>
-										</h3>
-										<hr>
-										<p class="post__date">
-											{{ \Carbon\Carbon::instance($morePost->created_at)->toFormattedDateString() }}
-										</p>
-									</header>
-									<main class="post__body">
-										{{ str_limit($morePost->body, 100, '...') }}
-									</main>
-									<footer class="post__footer">
-										<a href="/posts/{{ $morePost->slug }}" class="post__footer__link">Read this blog entry</a>
-									</footer>
-								</article>
-							</li>
-						@endforeach
-					</ul>
-				</section>
-				<section>
-					<h3 class="viewmore__heading">More works</h3>
-					<ul class="viewmore__works">
-						@foreach($moreWorks as $moreWork)
-							<li>
-								<img src="{{ asset($moreWork->thumb_path) }}" alt="" />
-								{{ $moreWork->title }}
-							</li>
-						@endforeach
-					</ul>
-				</section>
 			</div>
-		</div>
-	</main>
-</article>
+			<footer class="post__footer">
+				<a href="/posts/{{ $morePost->slug }}" class="post__footer__link">Read this blog entry</a>
+			</footer>
+		</article>
+		@endforeach
+		<a href="#" class="btn">
+			<span class="btn__text">View more blog posts</span>
+			<span class="btn__bg"></span>
+		</a>
+	</div>
+</section>
+@stop
+
+@section('scripts')
+	<script type="text/javascript" src="{{ asset( '/js/prettify/src/prettify.js' ) }}"></script>
+	<script type="text/javascript">
+		window.addEventListener( "load", function(){
+			var pre = [].slice.call( document.getElementsByTagName( "pre" ) );
+			pre.forEach( function( pre ){
+				pre.classList.add( "prettyprint" );
+				pre.classList.add( "theme-tonic" );
+			} );
+			prettyPrint();
+		} );
+	</script>
 @stop
