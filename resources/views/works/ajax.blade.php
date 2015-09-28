af1d1cdac44817875b3419b3cee2d21c2dd63580
Adrien Leloup <nittiyh@outlook.com>
2015-09-28 19:15:01 +0200

    Add morre works view (ajax)

---
+++ resources/views/works/ajax.blade.php
@@ -0,0 +1,17 @@
+@foreach($works as $moreWork)
+	<article class="row-3 work-preview appear">
+		<div class="hidden works-slug">{{ $moreWork->slug }}</div>
+		<div class="work-preview__thumb-container">
+			<a href="/works/{{ $moreWork->slug }}" class="work-preview__link">
+				<img src="{{ asset( $moreWork->thumbnail_path ) }}" class="work-preview__thumbnail" alt="" width="100%"/>
+			</a>
+		</div>
+		<h3 class="work-preview__heading">
+			<a href="/works/{{ $moreWork->slug }}" class="work-preview__link">{{ $moreWork->title }}</a>
+		</h3>
+		<hr>
+		<time class="work-preview__date">
+			{{ \Carbon\Carbon::instance($moreWork->created_at)->toFormattedDateString() }}
+		</time>
+	</article>
+@endforeach
