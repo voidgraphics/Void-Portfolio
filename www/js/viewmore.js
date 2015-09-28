1154808873c5cdd26ebc381d713ba47a87073365
Adrien Leloup <nittiyh@outlook.com>
2015-09-28 19:15:45 +0200

    Add ajax call to viewmore script

--- www/js/viewmore.js
+++ www/js/viewmore.js
@@ -2,23 +2,60 @@
 
 	"use strict";
 
-	var $viewMoreWorks = document.querySelector( "#viewmore-works" ),
-		$viewMorePosts = document.querySelector( "#viewmore-posts" );
+	var $viewMoreWorks = document.querySelector( "#viewmore-work" ),
+		$viewMorePosts = document.querySelector( "#viewmore-post" );
+		var err;
+
+	var appendWork = function( oData ){
+		var $btn = $( ".recent-works .btn-container" );
+		$btn.before( oData );
+	};
+
+	var appendPost = function( oData ){
+		var $btn = $( ".blog .btn-container" );
+		$btn.before( oData );
+	}
+
+	var retrieveMore = function( type ){
+		var x = [].slice.call(document.querySelectorAll( "." + type + "-slug" ));
+		var excludes = [];
+		x.forEach( function( excludeElement ){
+			excludes.push( excludeElement.innerHTML );
+		} );
+		console.log(excludes);
+		var excludesString = excludes.join("SEPARATOR");
+		var url = '/' + type + "/getmore/" + excludesString;
+		$.ajax( {
+			url: url,
+			method: "get",
+			data: {},
+			dataType: "html",
+			error: function( xhr, status, error ){
+				console.error(error);
+			},
+			success: function( oResponse ){
+				var containerSelector;
+				( type === "posts" ) && ( appendPost( oResponse ) );
+				( type === "works" ) && ( appendWork( oResponse ) );
+			},
+		} );
+	};
 
 	var showMore = function( e ){
 		e.preventDefault();
 
 		var target,
 			parent,
-			childNodes;
+			childNodes,
+			hiddenCount = 0;
 
-		if( e.target.id != "viewmore-posts" && e.target.id != "viewmore-works" ){
+		if( e.target.id != "viewmore-post" && e.target.id != "viewmore-work" ){
 			target = e.target.parentNode;
 		} else {
 			target = e.target;
 		}
 
-		parent = target.parentNode;
+		parent = target.parentNode.parentNode;
 		childNodes = [].slice.call(parent.childNodes);
 
 		childNodes.forEach( function( node ){
@@ -26,9 +63,17 @@
 				var style = window.getComputedStyle(node);
 				if( style.display === "none" ){
 					node.classList.add( "appear" );
+					hiddenCount++;
 				}
 			}
 		} );
+
+		if( hiddenCount === 0 ){
+			var type;
+			( target.id === "viewmore-post" ) && ( type = "posts" );
+			( target.id === "viewmore-work" ) && ( type = "works" );
+			retrieveMore( type );
+		}
 
 	};
 
