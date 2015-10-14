bbe98ca605857dc80affba21ba09d4b4284418b6
Adrien Leloup <contact@void.graphics>
2015-10-14 07:46:52 +0200

    Comment register routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,5 +28,5 @@ Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
